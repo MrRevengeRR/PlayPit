3e23d52f3396113c184ec8ff213f5057a276640e
MrRevengeRR <alex22.miculescu@gmail.com>
2023-03-16 10:18:09 +0200

    Update (Impius)

--- Impius/scripts/Player.gd
+++ Impius/scripts/Player.gd
@@ -4,8 +4,10 @@ export var SPEED = 30
 
 var velocity = Vector2.ZERO
 
+onready var animationPlayer = $AnimationPlayer
+
 func _ready():
-	Engine.set_target_fps(30)
+	Engine.set_target_fps(30) #de adaugat la node-urile principale (level scenes)
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -14,6 +16,10 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if (input_vector != Vector2.ZERO):
+		if (input_vector.x > 0):
+			animationPlayer.play("RunRight")
+		else:
+			animationPlayer.play("RunLeft")
 		velocity = input_vector * SPEED
 	else:
 		velocity = Vector2.ZERO
